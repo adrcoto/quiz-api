7c2f485573b7b4a6712683e9a339d900f3290853
Adrian Constantin <adrcoto@yahoo.com>
2020-03-08 18:27:32 +0200

    changed to prod srv

--- src/Server.js
+++ src/Server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
-const https = require('https');
-const fs = require('fs');
+/*const https = require('https');
+const fs = require('fs');*/
 require('./database/Mongoose');
 
 const userRoutes = require('./routes/UserRoutes');
@@ -9,11 +9,15 @@ const userRoutes = require('./routes/UserRoutes');
 let app = express();
 app.use(express.json());
 
-const key = fs.readFileSync(`${__dirname}/../cert/server.key`);
+/*const key = fs.readFileSync(`${__dirname}/../cert/server.key`);
 const cert = fs.readFileSync(`${__dirname}/../cert/server.crt`);
 
-let server = https.createServer({ key, cert }, app);
+let server = https.createServer({ key, cert }, app);*/
 
+
+app.listen(parseInt(process.env.PORT), () => {
+
+});
 
 /**
  * Endpoints
@@ -21,6 +25,6 @@ let server = https.createServer({ key, cert }, app);
 
 app.use(userRoutes);
 
-server.listen(parseInt(process.env.PORT), () => {
+/*server.listen(parseInt(process.env.PORT), () => {
     console.log('Success!', `Secured development server started on port ${process.env.PORT}`);
-});
+});*/
